e7909aa037e1af9fe7c0bceda1b9d9e96907edce
Divyam <divyam2203@gmail.com>
2024-06-27 05:17:38 +0530

    updating total progress

---
+++ src/questions2.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    id: "q1",
+    text: "Which of the following is NOT a core principle of object-oriented programming (OOP)?",
+    answers: ["Compilation", "Inheritance", "Encapsulation", "Abstraction"],
+  },
+  {
+    id: "q2",
+    text: "A linked list is a data structure where each element (node) contains data and a reference to the:",
+    answers: [
+      "Next element only",
+      "Previous element only",
+      "Both previous and next elements",
+      "No references to other elements",
+    ],
+  },
+  {
+    id: "q3",
+    text: "What is the time complexity of a linear search algorithm in the best-case scenario?",
+    answers: ["O(1)", "O(log n)", "O(n)", "O(n^2)"],
+  },
+  {
+    id: "q4",
+    text: "In a binary search tree, each node's value must be:",
+    answers: [
+      "Greater than all its children",
+      "Less than all its children",
+      "Equal to all its children",
+      "No restriction on the value compared to children",
+    ],
+  },
+  {
+    id: "q5",
+    text: "What is the difference between a compiler and an interpreter?",
+    answers: [
+      "A compiler translates code into machine code once, while an interpreter translates it line by line during execution.",
+      "A compiler is for high-level languages, while an interpreter is for low-level languages.",
+      "A compiler is faster, while an interpreter is more flexible.",
+      "There is no practical difference.",
+    ],
+  },
+  {
+    id: "q6",
+    text: "Which of the following is NOT a valid data type in Python?",
+    answers: ["Array", "Integer", "Float", "String"],
+  },
+  {
+    id: "q7",
+    text: "The process of converting analog signals (continuous) into digital signals (discrete) is called:",
+    answers: ["Digitization", "Encryption", "Decryption", "Compression"],
+  },
+  {
+    id: "q8",
+    text: "In computer networking, what is the role of a firewall?",
+    answers: [
+      "To control and filter incoming and outgoing network traffic",
+      "To connect devices within a network",
+      "To provide a user interface for internet access",
+      "To store and manage website content",
+    ],
+  },
+  {
+    id: "q9",
+    text: "What is the term for a software development methodology that emphasizes iterative development, testing, and feedback loops?",
+    answers: [
+      "Agile development",
+      "Waterfall model",
+      "Spiral model",
+      "Prototype model",
+    ],
+  },
+  {
+    id: "q10",
+    text: "In web development, what language is used to define the structure and content of a web page?",
+    answers: ["HTML", "CSS", "JavaScript", "Python"],
+  },
+];
